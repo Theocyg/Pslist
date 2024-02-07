8766f4b25c833400119f16e9150b038a4295f7d1
Théo/ <80842242+Theocyg@users.noreply.github.com>
2024-02-07 15:31:05 +0100

    V2
    J'ai mis l'ancienne version a la place du _old et je bosse sur les arguments

--- pslist.c
+++ pslist.c
@@ -3,15 +3,16 @@
 #include<tchar.h>
 #include<windows.h>
 #include<tlhelp32.h>
+
 int _tmain(int argc, TCHAR *argv[]) {
-	HANDLE hProcessSnap =
-		CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 	//snapshot all process
 	if (hProcessSnap == INVALID_HANDLE_VALUE) {
 		_tprintf(_T("Pslist error \n"));
 		exit(EXIT_FAILURE);
 	}
+
 	PROCESSENTRY32 pe32;
 	pe32.dwSize = sizeof(PROCESSENTRY32);
 
@@ -21,12 +22,63 @@ int _tmain(int argc, TCHAR *argv[]) {
 		CloseHandle(hProcessSnap);
 		exit(EXIT_FAILURE);
 	}
-		
-	_tprintf(_T("\t[Name] \t[PID]\t[ThreadID]\t[PPID] \n")); // https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/ns-tlhelp32-processentry32
-	//print all process information
-	do {
-		_tprintf(_T("%25s %8d %8d %8d \n"),
-			pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads, pe32.th32ParentProcessID);
-	} while (Process32Next(hProcessSnap, &pe32));
+
+	_tprintf(_T("\t[Name] \t[PID]\t[ThreadID]\t[PPID]\t[CPUTime]\t[UserTime] \n"));
+
+	if (_tcscmp(argv[1], _T("-h")) == 0) {
+		_tprintf(_T("Usage: pslist [option] \n"));
+		exit(EXIT_FAILURE);
+	} else if (argc > 2 && _tcscmp(argv[1], _T("-d")) == 0) {
+		DWORD targetPID = _ttoi(argv[2]);
+		do {
+			if (pe32.th32ProcessID == targetPID) {
+				HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pe32.th32ProcessID);
+				if (hProcess != NULL) {
+					FILETIME ftCreation, ftExit, ftKernel, ftUser;
+					if (GetProcessTimes(hProcess, &ftCreation, &ftExit, &ftKernel, &ftUser)) {
+						// Convert FILETIME to ULONGLONG
+						ULONGLONG ullKernel = (((ULONGLONG)ftKernel.dwHighDateTime) << 32) + ftKernel.dwLowDateTime;
+						ULONGLONG ullUser = (((ULONGLONG)ftUser.dwHighDateTime) << 32) + ftUser.dwLowDateTime;
+
+						// Convert to seconds
+						double dKernel = ullKernel * 1e-7;
+						double dUser = ullUser * 1e-7;
+
+						_tprintf(_T("\t%s\t%d\t%d\t%d\t%f\t%f\n"),
+							pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads, pe32.th32ParentProcessID, dKernel, dUser);
+					} else {
+						_tprintf(_T("GetProcessTimes error ! \n"));
+					}
+					CloseHandle(hProcess);
+				}
+				break;
+			}
+		} while (Process32Next(hProcessSnap, &pe32));
+	} else if (argv[1] == NULL){
+		//print all process information
+		do {
+			HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pe32.th32ProcessID);
+			if (hProcess != NULL) {
+				FILETIME ftCreation, ftExit, ftKernel, ftUser;
+				if (GetProcessTimes(hProcess, &ftCreation, &ftExit, &ftKernel, &ftUser)) {
+					// Convert FILETIME to ULONGLONG
+					ULONGLONG ullKernel = (((ULONGLONG)ftKernel.dwHighDateTime) << 32) + ftKernel.dwLowDateTime;
+					ULONGLONG ullUser = (((ULONGLONG)ftUser.dwHighDateTime) << 32) + ftUser.dwLowDateTime;
+
+					// Convert to seconds
+					double dKernel = ullKernel * 1e-7;
+					double dUser = ullUser * 1e-7;
+
+					_tprintf(_T("\t%s\t%d\t%d\t%d\t%f\t%f\n"),
+						pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads, pe32.th32ParentProcessID, dKernel, dUser);
+				} else {
+					_tprintf(_T("GetProcessTimes error ! \n"));
+				}
+				CloseHandle(hProcess);
+			}
+		} while (Process32Next(hProcessSnap, &pe32));
+	}
+
+	CloseHandle(hProcessSnap);
 	return 0;
 }
