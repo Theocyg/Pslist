1431faff0c43a88f17568c331cf89bd3178ad932
Théo Cygan <theo.cygan@ecole2600.com>
2024-02-18 22:41:57 +0100

    Final

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Pslist
-Process List en C
+# Plist.exe
+
+Plist is used to List the process of your windows machine by listing them,  sorted by name, PID, threadID, PPID.
+
+To use it, just execute it from your cmd by using ./<PATH TO THE EXE>
+
+By default, without arguments, the program will list every process
+
+## Arguments:
+* `-d <PID>`: Will list process with the PID you used as argument
+* `-n <ProcessName>`: Will list process with the name you used as argument (🚧 Work in progress... 🚧)
+* `-h`: Will show the arguments you can use 
--- pslist.c
+++ pslist.c
@@ -1,32 +1,133 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<tchar.h>
-#include<windows.h>
-#include<tlhelp32.h>
-int _tmain(int argc, TCHAR *argv[]) {
-	
-	HANDLE hProcessSnap =
-		CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+#include <tchar.h>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	//snapshot all process
-	if (hProcessSnap == INVALID_HANDLE_VALUE) {
-		_tprintf(_T("Pslist error \n"));
-		exit(EXIT_FAILURE);
-	}
-	PROCESSENTRY32 pe32;
-	pe32.dwSize = sizeof(PROCESSENTRY32);
+int _tmain(int argc, TCHAR *argv[])
+{
 
-	//retrieve the process information
-	if (!Process32First(hProcessSnap, &pe32)) {
-		_tprintf(_T("Process32First error ! \n"));
-		CloseHandle(hProcessSnap);
-		exit(EXIT_FAILURE);
-	}
-		
-	_tprintf(_T("\t[PID]\t[Name]\t[ThreadID]\t[PPID]\n"));	//print all process information
-		do {
-			_tprintf(_T("\t%d\t%s\t%d\t%d\n"),
-							pe32.th32ProcessID,pe32.szExeFile, pe32.cntThreads, pe32.th32ParentProcessID);
-		} while (Process32Next(hProcessSnap, &pe32));
-		return 0;
+    if (argc == 1)
+    {
+        // no arguments, show all processes
+        HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+        if (hProcessSnap == INVALID_HANDLE_VALUE)
+        {
+            // error
+            _tprintf(_T("Pslist error \n"));
+            exit(EXIT_FAILURE);
+        }
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+
+        _tprintf(_T("\t[Name] \t[PID]\t[ThreadID]\t[PPID] \n"));
+
+        if (Process32First(hProcessSnap, &pe32))
+        {
+            // loop through all processes
+            do
+            {
+                // print process stats
+                _tprintf(_T("%25s %8d %8d %8d \n"),
+                         pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads, pe32.th32ParentProcessID);
+            } while (Process32Next(hProcessSnap, &pe32));
+        }
+
+        CloseHandle(hProcessSnap);
+    }
+    else if (argc == 3 && strcmp(argv[1], "-d") == 0)
+    {
+        // CODE DU <-d>
+    }
+
+    else if (argc >= 3 && _tcscmp(argv[1], _T("-d")) == 0)
+    {
+        // -d argument to show process stats for the specified PID
+        int pid = _ttoi(argv[2]);
+
+        HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+        if (hProcessSnap == INVALID_HANDLE_VALUE)
+        {
+            // error
+            _tprintf(_T("Pslist error \n"));
+            exit(EXIT_FAILURE);
+        }
+
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+
+        if (Process32First(hProcessSnap, &pe32))
+        {
+            // loop through all processes
+            do
+            {
+                // print process stats for the specified PID
+                if (pe32.th32ProcessID == pid)
+                {
+                    _tprintf(_T("\t[Name] \t[PID]\t[ThreadID]\t[PPID] \n"));
+                    _tprintf(_T("%25s %8d %8d %8d \n"),
+                             pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads, pe32.th32ParentProcessID);
+                    break;
+                }
+            } while (Process32Next(hProcessSnap, &pe32));
+        }
+
+        CloseHandle(hProcessSnap);
+    }
+
+    // else if (argc >= 3 && _tcscmp(argv[1], _T("-n")) == 0)
+    // {
+    //     // -n argument to show process stats for the specified process name
+    //     TCHAR *processName = argv[2];
+
+    //     HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    //     if (hProcessSnap == INVALID_HANDLE_VALUE)
+    //     {
+    //         // error
+    //         _tprintf(_T("Pslist error \n"));
+    //         exit(EXIT_FAILURE);
+    //     }
+
+    //     PROCESSENTRY32 pe32;
+    //     pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    //     if (!Process32First(hProcessSnap, &pe32))
+    //     {
+    //         // error
+    //         _tprintf(_T("Pslist error \n"));
+    //         CloseHandle(hProcessSnap);
+    //         exit(EXIT_FAILURE);
+    //     }
+
+    //     do
+    //     {
+    //         if (_tcscmp(pe32.szExeFile, processName) == 0)
+    //         {
+    //             // print process info
+    //             _tprintf(_T("Process Name: %s\n"), pe32.szExeFile);
+    //             _tprintf(_T("Process ID: %d\n"), pe32.th32ProcessID);
+    //             // add more info as needed
+    //         }
+    //     } while (Process32Next(hProcessSnap, &pe32));
+
+    //     CloseHandle(hProcessSnap);
+    // }
+
+    else if (_tcscmp(argv[1], _T("-h")) == 0)
+    {
+        // -h argument to show all usable arguments for the program
+        _tprintf(_T("Usable arguments:\n"));
+        _tprintf(_T("-d <PID>: Show process stats for the specified PID\n"));
+        _tprintf(_T("-h: Show all usable arguments for the program\n"));
+    }
+    else
+    {
+        // invalid arguments
+        _tprintf(_T("Invalid arguments. Use -h to show all usable arguments.\n"));
+    }
+
+    return 0;
 }
